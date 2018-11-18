f27f913f832ec4b0cea31b3be61298a27d34b6e2
iMusi <lomusive@outlook.com>
2018-11-18 20:19:08 +0800

    Update ssrmu.sh

--- ssrmu.sh
+++ ssrmu.sh
@@ -877,7 +877,7 @@ Download_SSR(){
 }
 Service_SSR(){
 	if [[ ${release} = "centos" ]]; then
-		if ! wget --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/service/ssrmu_centos -O /etc/init.d/ssrmu; then
+		if ! wget --no-check-certificate https://raw.githubusercontent.com/iMusi/doubi/master/service/ssrmu_centos -O /etc/init.d/ssrmu; then
 			echo -e "${Error} ShadowsocksR服务 管理脚本下载失败 !" && exit 1
 		fi
 		chmod +x /etc/init.d/ssrmu
